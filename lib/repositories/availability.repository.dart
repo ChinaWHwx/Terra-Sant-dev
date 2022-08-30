39b8fbec1dafd4c48edf58ad6ad60c86f0974d19
XiangyuCAO <alain.cxy@gmail.com>
2022-08-30 15:34:44 +0200

    change url to server

--- lib/repositories/availability.repository.dart
+++ lib/repositories/availability.repository.dart
@@ -4,7 +4,7 @@ class AvailabilityRepository extends GetConnect {
   @override
   void onInit() {
     //httpClient.defaultDecoder = Availability.availabilityFromJson;
-    httpClient.baseUrl = 'http://192.168.1.161:5000';
+    httpClient.baseUrl = 'http://51.178.83.92:5000';
   }
 
   Future addAvl(body) => post("/availability/add", body);
