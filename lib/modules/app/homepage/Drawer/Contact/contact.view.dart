c143c14173c43ae88081560ce9451429534806dd
CxyAlain <alain.cxy@gmail.com>
2022-12-06 21:56:13 +0800

    ios version1

--- lib/modules/app/homepage/Drawer/Contact/contact.view.dart
+++ lib/modules/app/homepage/Drawer/Contact/contact.view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/modules/app/homepage/Drawer/Contact/contact.controller.dart';
 import 'package:get/get.dart';
 import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactView extends GetView<ContactController> {
   const ContactView({Key? key}) : super(key: key);
@@ -36,9 +37,9 @@ class ContactView extends GetView<ContactController> {
                       constraints: const BoxConstraints.tightFor(width: 200),
                       child: ElevatedButton(
                         child: const Text('Call'),
-                        onPressed: () {
-                          //launch('tel:0749660950');
-                          FlutterPhoneDirectCaller.callNumber("0749660950");
+                        onPressed: () async {
+                          // launchUrl(Uri.parse('tel:0749660950'));
+                          await FlutterPhoneDirectCaller.callNumber("0749660950");
                         },
                       )),
                 ],
